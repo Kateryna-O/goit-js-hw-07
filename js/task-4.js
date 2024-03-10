41f7822c9e55b7ac373c09303b4379764cfee91b
Kateryna-O <osadcukkata88@gmail.com>
2024-03-10 20:54:06 +0200

    done task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const emailInput = registerForm.elements.email;
+  const passwordInput = registerForm.elements.password;
+
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const formData = {
+      email: emailValue,
+      password: passwordValue,
+    };
+    console.log(formData);
+    registerForm.reset();
+  }
+});
