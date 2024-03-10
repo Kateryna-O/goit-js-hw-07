f92ed4051d8b0d699521245417ec09513a0d5678
Kateryna-O <osadcukkata88@gmail.com>
2024-03-10 20:54:22 +0200

    done task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const changeBtn = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+const body = document.body;
+
+changeBtn.addEventListener("click", () => {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  body.style.backgroundColor = getRandomHexColor();
+  colorSpan.textContent = getRandomHexColor();
+});
